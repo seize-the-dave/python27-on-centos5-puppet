12ec08d2799ecae95929547e50cbc491f700422f
David Grant <david@grant.org.uk>
2012-12-13 08:38:16 +0000

    Initial puppet manifest to install Python 2.7 on CentOS 5.x

---
+++ python27.pp
@@ -0,0 +1,43 @@
+package {"epel-release":
+	ensure		=>	installed,
+	source		=>	"http://www.mirrorservice.org/sites/dl.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm",
+	provider	=>	rpm
+}
+
+$packages = [ "rpmdevtools", "tk-devel", "tcl-devel", "expat-devel", "db4-devel", "gdbm-devel", "sqlite-devel", "bzip2-devel", "openssl-devel", "ncurses-devel", "readline-devel" ]
+package {$packages:
+	ensure	=>	installed,
+	before	=>	Exec["rpmdev-setuptree"],
+	require	=>	Package["epel-release"]
+}
+
+exec {"rpmdev-setuptree":
+	path		=>	"/bin:/usr/bin",
+	command		=>	"rpmdev-setuptree",
+	before		=>	Exec["get python.spec"],
+}
+
+exec {"get python.spec":
+	path		=>	"/bin:/usr/bin",
+	command		=>	"wget --no-check-certificate https://raw.github.com/nmilford/specfiles/master/python-2.7/python27-2.7.2.spec -O /usr/src/redhat/SPECS/python27-2.7.2.spec",
+	before		=>	Exec["get python"],
+}
+
+exec {"get python":
+	path	=>	"/bin:/usr/bin",
+	command	=>	"wget http://www.python.org/ftp/python/2.7.2/Python-2.7.2.tar.bz2 -O /usr/src/redhat/SOURCES/Python-2.7.2.tar.bz2",
+	before	=>	Exec["rpmbuild"],
+}
+
+exec {"rpmbuild":
+	path		=>	"/bin:/usr/bin",
+	command		=>	"QA_RPATHS=$[ 0x001|0x0010] rpmbuild -bb /usr/src/redhat/SPECS/python27-2.7.2.spec",
+	before		=>	Package["python27"],
+	timeout		=>	0,
+}
+
+package {"python27":
+	ensure		=>	"installed",
+	provider	=>	"rpm",
+	source		=>	"/usr/src/redhat/RPMS/x86_64/python27*.rpm"
+}
